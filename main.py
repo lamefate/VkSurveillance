c0a382e7000aab29122648b86d0298caf0890225
Lame Fate <119226474+lamefate@users.noreply.github.com>
2022-11-27 22:24:06 +0300

    update main class

--- main.py
+++ main.py
@@ -1,32 +1,34 @@
-from json import dumps, loads
-from random import randint
-from time import sleep
+from auth import Auth
 from os import path
 import configparser
-import vk_api
 
 
-class MainMenu:
+class Main:
     def __init__(self):
         self.config_filename = 'config.ini'
         self.params = self.load_config(self.config_filename)
+        self.auth_process = Auth(self.params)
 
     def generate_config(self, filename):
         # setting parameters and sections
         config = configparser.ConfigParser()
         config.add_section('Auth')
-        config.set('Auth', 'Phone', '+7')
-        config.set('Auth', 'Password', 'q')
+        config.set('Auth', 'phone', '+7')
+        config.set('Auth', 'password', 'q')
 
         # save config to .ini file
         with open(filename, 'w') as config_file:
             config.write(config_file)
 
+        print('info: config.ini created')
+
     def load_config(self, filename):
         # if config.ini doesn't exist
         if not path.exists(filename) and not path.isfile(filename):
+            print('info: config.ini does not exist, creating...')
             self.generate_config(filename)
 
+        print('info: loading config.ini params')
         # read config file
         config = configparser.ConfigParser()
         config.read(filename)
@@ -34,3 +36,5 @@ class MainMenu:
         # return read config
         return config
 
+
+Main()
