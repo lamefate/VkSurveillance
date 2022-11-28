00772858d8ddae96c01ba4ef4e0f40644207504a
Lame Fate <119226474+lamefate@users.noreply.github.com>
2022-11-28 08:03:19 +0300

    create empty functions

--- surveillance.py
+++ surveillance.py
@@ -5,4 +5,16 @@ import vk_api
 class Surveillance:
     def __init__(self, api, params):
         self.api = api
+        self.timeout = params['Surveillance']['api_timeout']
 
+    def friends(self):
+        pass
+
+    def posts(self):
+        pass
+
+    def comments(self):
+        pass
+
+    def online(self):
+        pass
